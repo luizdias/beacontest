cfa42abd65a70a9046a13c26b3869c970a289230
Luiz Fernando Dias <luizfdias@macbook.local>
2019-02-21 12:01:41 -0300

    First commit with all the files for this project

---
+++ BeaconTest/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  BeaconTest
+//
+//  Created by Luiz Fernando Aquino Dias on 15/01/18.
+//  Copyright © 2018 Town Tree. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+
+}
+
